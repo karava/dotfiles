bdc21d76da40fba1773b3ca8dd51c2cd99ba2464
Kishan Arava <kishan.arava@gmail.com>
2020-09-28 09:38:33 +1000

    Setup dotfiles for stow

---
+++ README.md
@@ -0,0 +1,12 @@
+## Kishan Arava dotfiles
+
+Custom configuration files for:
+* Bash
+* Zsh
+* Git
+* Tmux
+* Vim
+* Custom scripts
+
+There is also a script for easy simlinking the dotfiles
+
---
+++ setup-dotfiles.sh
@@ -0,0 +1,40 @@
+#!/usr/bin/env bash
+
+# set -e          # Exit on error
+# set -o pipefail # Exit on pipe error
+# set -x          # Enable verbosity
+
+# Dont link DS_Store files
+find . -name ".DS_Store" -exec rm {} \;
+
+PROGRAMS=(alias git scripts stow tmux vim zsh)
+OLD_DOTFILES="dotfile_bk_$(date -u +"%Y%m%d%H%M%S")"
+mkdir $OLD_DOTFILES
+
+function backup_if_exists() {
+    if [ -f $1 ];
+    then
+      mv $1 $OLD_DOTFILES
+    fi
+    if [ -d $1 ];
+    then
+      mv $1 $OLD_DOTFILES
+    fi
+}
+
+# Clean common conflicts
+backup_if_exists ~/.bash_profile
+backup_if_exists ~/.bashrc
+backup_if_exists ~/.gitconfig
+backup_if_exists ~/.tmux.conf
+backup_if_exists ~/.zshrc
+backup_if_exists ~/.zprofile
+
+mkdir -p ~/.vim/undodir
+
+for program in ${PROGRAMS[@]}; do
+  stow -v --target=$HOME $program
+  echo "Configuring $program"
+done
+
+echo "Done!"
