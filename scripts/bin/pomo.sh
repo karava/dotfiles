f27f9a944e27355f6c7e5ec33d77d0503eac48fd
karava <kishan.arava@gmail.com>
2025-04-01 06:06:20 +0800

    Update pomo script to include music

--- scripts/bin/pomo.sh
+++ scripts/bin/pomo.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 # A Pomodoro timer for the macOS terminal.
+# Usage: ./pomo.sh <duration in minutes> [--music]
 
 set -e
 
@@ -11,8 +12,21 @@ then
 fi
 
 minutes=$1
+shift
+
+# Check for the --music flag
+play_music=false
+if [ "$1" == "--music" ]; then
+    play_music=true
+fi
 
 echo "Starting a Pomodoro timer of $minutes minutes."
+
+# If music flag is set, start palying work music.
+if $play_music; then
+    echo "Starting work music..."
+    osascript -e 'tell application "Music" to play playlist "Work Music"'
+fi
 
 i=0
 while test $i -lt $minutes
@@ -22,8 +36,14 @@ do
     echo $i minutes elapsed
 done
 
-echo "Hey Pomodoro, time's up!"
-say "hey pomodoro, times up, you have a minute left"
+# If music flag is set, stop the music at the end.
+if $play_music; then
+    echo "Stopping work music..."
+    osascript -e 'tell application "Music" to pause'
+fi
+
+echo "Hey Kish, time's up!"
+say "hey Kish, times up, you have a minute left"
 
 sleep 50
 ~/bin/macAlerts.sh 'closing in 10seconds'
